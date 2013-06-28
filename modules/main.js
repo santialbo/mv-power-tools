3e265dd1592bc001b4e132608e719e30c38aa5d6
santi <santialbo@gmail.com>
2013-06-28 13:44:40 +0200

    Add shortcuts module

--- modules/main.js
+++ modules/main.js
@@ -6,6 +6,8 @@
 // @grant       GM_addStyle
 // @include     http://www.mediavida.com/*
 // @require     http://code.jquery.com/jquery-1.10.1.min.js
+// @require     http://cdn.craig.is/js/mousetrap/mousetrap.min.js
+// @require     http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.4/underscore-min.js
 // ==/UserScript==
 
 window.PowerTools = {
