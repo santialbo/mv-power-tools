8b6464bc5a6f7f0a7c4da803252bb6bda5149ad8
santi <santialbo@gmail.com>
2013-07-03 16:04:25 +0100

    Add button that enables to see the user info without leaving the thread

--- modules/author-enhancements.js
+++ modules/author-enhancements.js
@@ -1,7 +1,24 @@
 (function($, PT) {
     $(function() {
+        // author-enhancements only works in threads
+        if ($('#tid').length == 0) return;
+
+        var getUserInfo = function(userId) {
+            var url = PowerTools.url + '/id/' + userId;
+            return $.get(url).then(function(source) {
+                var div = $('div.userinfo', source);
+                return {
+                    personalInfo: / *(.*),(?:\n +\t+(-?\d+) años, +(.*))?/.exec(div.children()[0].textContent).slice(1),
+                    lastSeen: /((?:online)|(?:visto hace \d+ [^ ]+))/.exec(div.children()[1].textContent)[0].replace('visto hace ', ''),
+                    registered: /miembro desde +(\d+ [^ ]+ \d+)/.exec(div.children()[2].textContent)[1],
+                    forumInfo: /(\d+) posts \| (\d+) visitas \| (\d+) firmas/.exec(div.children()[3].textContent).slice(1).map(function(n) { return parseInt(n); }),
+                };
+            });
+        };
+
         var enhanceAuthor = function(posts) {
             posts.find('.autor').each(function() {
+                if ($(this).find('a:first').length == 0) return;
                 var authorId = $(this).find('a:first').attr('href').match(/\/id\/(.*)/)[1];
                 var online = $(this).find('dd.online').length > 0;
                 $(this).find('dd.online').remove();
@@ -16,8 +33,30 @@
                         '<div class="firma-pos">',
                             '<a class="tooltip pt-firmas sprite icon" href="/id/' + authorId + '/firmas" title="Firmas"></a>',
                         '</div>',
+                        '<div class="info-pos">',
+                            '<a class="tooltip pt-info" title="Info" href="#" rel="' + authorId + '"><strong>+</strong></a>',
+                        '</div>',
                     '</div>'].join(''));
                 $(this).append(enhancements);
+            });
+            $('a.pt-info', posts).click(function() {
+                var userId = $(this).attr('rel');
+                var authorDiv = $(this).parent().parent().parent();
+                getUserInfo(userId).then(function(info) {
+                    authorDiv.append([
+                        '<dl class="userinfo">',
+                            '<dd><strong>' + info.personalInfo[0] + '</strong>' + (info.personalInfo[1] ? ' (' + info.personalInfo[1] + ')' : '') + '</dd>',
+                            (info.personalInfo[2] ? '<dd>de ' + info.personalInfo[2] + '</dd>' : ''),
+                            '<dd>' + (info.lastSeen == 'online' ? '' : 'visto hace ') + info.lastSeen + '</dd>',
+                            '<dd>registro el ' + info.registered + '</dd>',
+                            '<dd>' + info.forumInfo[0] + ' posts</dd>',
+                            '<dd>' + info.forumInfo[1] + ' visitas</dd>',
+                            '<dd><a href="/id/' + userId + '/firmas">' + info.forumInfo[2] + ' firmas</a></dd>',
+                        '</dl>',
+                        ].join(''));
+                });
+                $(this).contents().unwrap();
+                return false;
             });
         } 
 
@@ -74,6 +113,19 @@
                 'width: 19px;',
                 'height: 12px;',
                 'padding-top: 1px;',
+            '}',
+            '.info-pos {',
+                'float: left;',
+                'width: 19px;',
+                'height: 12px;',
+                'padding-top: 1px;',
+                'margin-top: -4px;',
+                'font-size: 120%;',
+            '}',
+            '.userinfo {',
+                'float: left;',
+                'font-size: 10px;',
+                'padding-top: 5px;',
             '}',
         ].join('');
         $('head').append('<style type="text/css">' + css + '</style>');
